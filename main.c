e6ae59fd6d5a348e2926daa5cf02e61ff56aa406
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-09-22 04:41:37 +0300

    Fix loop loading overtime to outside
    Once all textures were loaded, no need to go over to load again to avoid CPU increase.
    
    - Martin Eesmaa

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Helga's Cheese Festival (Martin Eesmaa)");
     SetTargetFPS(60);
+    Texture2D background = LoadTexture("img/gback.png");
+        
+    Image girl1 = LoadImage("img/girl1.png");
+    ImageResize(&girl1, 188, 350);
+    Texture2D girl = LoadTextureFromImage(girl1);
+    UnloadImage(girl1);
+
+    Image boy1 = LoadImage("img/boy1.png");
+    ImageResize(&boy1, 188, 400);
+    Texture2D boy = LoadTextureFromImage(boy1);
+    UnloadImage(boy1);
+
     while (!WindowShouldClose())
     {
-        Texture2D background = LoadTexture("img/gback.png");
-        
-        Image girl1 = LoadImage("img/girl1.png");
-        ImageResize(&girl1, 188, 350);
-        Texture2D girl = LoadTextureFromImage(girl1);
-        UnloadImage(girl1);
-
-        Image boy1 = LoadImage("img/boy1.png");
-        ImageResize(&boy1, 188, 400);
-        Texture2D boy = LoadTextureFromImage(boy1);
-        UnloadImage(boy1);
-
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(background, 0, 0, WHITE);
