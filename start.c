b6b4d41b6194bf422833d42800e6e2a8d626a4c9
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-10-20 09:26:23 +0300

    Implement static transition texts frame
    This will help when the ENTER key or the gamepad face down button is released by moving to the next text.
    
    This will add a number move to the following text after the first text disappears.
    
    - Martin Eesmaa

--- start.c
+++ start.c
@@ -6,12 +6,26 @@ int start(void)
     Texture2D textbox = LoadTexture("img/textbox.png");
     Font atp = LoadFontEx("font/All Things Pink.ttf", 29, 0, 0);
 
+    int framesample = 0;
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BLACK);
         DrawTexture(textbox, 0, GetScreenHeight() - textbox.height, WHITE);
-        TypeTextEx(atp, "Sorry, it's not implemented yet. Coming soon... - Martin Eesmaa", (Vector2){ 20, GetScreenHeight() - textbox.height + 20 }, atp.baseSize, 0, WHITE);
+        const char* experimental = "Experimental version by Martin Eesmaa";
+        DrawText(experimental, GetScreenWidth() - MeasureText(experimental, 20), 0, 20, WHITE);
+
+        if(IsKeyReleased(KEY_ENTER) || IsGamepadButtonReleased(0, GAMEPAD_BUTTON_RIGHT_FACE_DOWN)) {
+            framesample += 1;
+        }
+
+        if(framesample == 0) {
+            TypeTextEx(atp, "Sorry, it's not implemented yet. Coming soon... - Martin Eesmaa", (Vector2){ 20, GetScreenHeight() - textbox.height + 20 }, atp.baseSize, 0, WHITE);
+        } else if(framesample == 1) {
+            TypeTextEx(atp, "Nice it's working!", (Vector2){ 20, GetScreenHeight() - textbox.height + 20 }, atp.baseSize, 0, WHITE);
+        }
+
         EndDrawing();
     }
     UnloadTexture(textbox);
