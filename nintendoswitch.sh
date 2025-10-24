f3a3045837b576662e12b6537da54d2de44afd17
MartinEesmaa <martin.eesmaa@protonmail.com>
2025-10-25 02:53:24 +0300

    Add support target for Nintendo Switch
    Note that I tested on Linux to use shell script by compile Nintendo Switch raylib version and worked okay
    
    - Martin Eesmaa

---
+++ nintendoswitch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+modify_source_code() {
+
+    if [ ! -d "romfs" ]; then
+        mkdir romfs
+    fi
+
+    cp -rf font romfs
+    cp -rf img romfs
+    cp -rf chapter romfs
+
+    if [ ! -d "source" ]; then
+        mkdir source
+    fi
+    cp -f *.c *.h source/
+
+    sed -i 's|img/|romfs:/img/|g' source/*.c
+    sed -i 's|font/|romfs:/font/|g' source/*.c
+    sed -i 's|chapter/|romfs:/chapter/|g' source/*.c
+}
+
+clean_build() {
+    rm -rf romfs source build
+    if [ -f Makefile.bak ]; then
+        mv Makefile Makefile.NX
+        mv Makefile.bak Makefile
+    fi
+    exit 1
+}
+
+handle_cancel() {
+    echo "Katkestatud, tagasi..."
+    clean_build
+}
+
+cp Makefile Makefile.bak
+cp Makefile.NX Makefile
+
+trap handle_cancel INT TERM
+
+modify_source_code
+
+if ! make PLATFORM=PLATFORM_NX; then
+    echo "Make vigu. Tagasi..."
+    clean_build
+fi
+
+clean_build
